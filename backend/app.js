65c5c12b3186f5a27fd728b84f04c68a2630920c
Sofia Lyvarenko <livarenko.sofia@gmail.com>
2023-04-02 14:43:20 +0300

    краш-тест сервера и редактирование README

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
